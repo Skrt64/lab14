4a0681b4741462f851ce3e58f3cf7f67464aadae
skrt <farm.shk@gmail.com>
2021-12-28 22:48:08 +0700

    lab14

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 void stat(const double[],int,double[]);
@@ -16,4 +17,35 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double a[],int N,double b[]){
+    double sum = 0;
+    double sd,gm;
+    double hmsum;
+    double max = a[0],min =a[0];
+    for(int i=0;i < N; i++){
+        sum += a[i];
+    }
+    b[0] = sum/N;
+
+    for(int i = 0;i < N;i++){
+        sd += pow((a[i]-b[0]),2);
+    }
+    b[1] = sqrt(sd/N);
+    for(int i = 0;i <N;i++){
+        gm += log10(a[i]);
+    }
+    b[2] = pow(10,gm/N);
+    for(int i = 0;i < N;i++){
+        hmsum += 1/a[i];
+    }
+    b[3] = N/hmsum;
+    for(int i =0; i<N;i++){
+        if(a[i]>max) max=a[i];
+        if(a[i]<min) min=a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+
 }
